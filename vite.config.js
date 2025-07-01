4ded33e689233ed97281b5bb80a062b2fc6344f5
Destiny Nguyen <destinynguyen@Destinys-MacBook-Air-2.local>
2025-07-01 10:24:25 -0700

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/surface-area/',
   server: {
     port: 8080,
     host: '0.0.0.0',
